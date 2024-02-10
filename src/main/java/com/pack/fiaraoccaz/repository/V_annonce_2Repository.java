751e800f56c9a50f99ee848688cb7a0834e3e0e0
nirs0204 <157597847+NirisoaLala@users.noreply.github.com>
2024-02-10 19:20:32 +0300

    annonce by user 2

--- src/main/java/com/pack/fiaraoccaz/repository/V_annonce_2Repository.java
+++ src/main/java/com/pack/fiaraoccaz/repository/V_annonce_2Repository.java
@@ -8,5 +8,6 @@ import com.pack.fiaraoccaz.model.V_Annonce_2;
 
 public interface V_annonce_2Repository extends JpaRepository<V_Annonce_2, Integer>{
     List<V_Annonce_2> findAllByEtatAndStatus(int etat, int status);
+    List<V_Annonce_2> findAllByEtatAndStatusAndIduser(int etat, int status, int iduser);
     
 }
