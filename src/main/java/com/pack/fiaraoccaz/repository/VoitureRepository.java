f1843d945998ee47782207b4d503d77742dc7e3c
nirs0204 <157597847+NirisoaLala@users.noreply.github.com>
2024-02-10 06:28:39 +0300

    debug

--- src/main/java/com/pack/fiaraoccaz/repository/VoitureRepository.java
+++ src/main/java/com/pack/fiaraoccaz/repository/VoitureRepository.java
@@ -2,6 +2,8 @@ package com.pack.fiaraoccaz.repository;
 
 import com.pack.fiaraoccaz.entity.Voiture;
 import com.pack.fiaraoccaz.entity.Annonce;
+import com.pack.fiaraoccaz.entity.User;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +12,7 @@ import java.util.List;
 
 @Repository
 public interface VoitureRepository extends JpaRepository<Voiture, Long> {
-    List<Voiture> findByUserId(Long userId);
+    List<Voiture> findAllByUtilisateur(User usesr);
     @Query("SELECT a FROM Annonce a WHERE a.voiture.utilisateur.iduser = :userId")
     List<Annonce> findAnnoncesByUtilisateurId(Long userId);
 
